e5aff8819f82a301bf85a631421a2676205f6e06
Micael Rodrigues <micaelr95@outlook.pt>
2019-02-24 16:45:35 +0000

    Prevent select and show message on console

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 document.addEventListener('contextmenu', function(e) {
     e.preventDefault();
 });
+
+document.addEventListener('pointerdown', function(e) {
+    e.preventDefault();
+});
+
+console.log('Hello there!');
+console.log('If you are interested in my work please contact me using one of the channels on the bottom of the page.');
