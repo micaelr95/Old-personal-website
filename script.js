072a5e8d21b3c5bdf521eda5f48b9133ef029078
Micael Rodrigues <micaelr95@outlook.pt>
2019-02-24 18:58:11 +0000

    Changed div margin from bottom to top and changed console message

--- script.js
+++ script.js
@@ -6,5 +6,5 @@ document.addEventListener('pointerdown', function(e) {
     e.preventDefault();
 });
 
-console.log('Hello there!');
-console.log('If you are interested in my work please contact me using one of the channels on the bottom of the page.');
+console.log('Hello there!\nI\'m a programmer (by that I mean I\'m lonely and poor), if you are interested in my work please contact me (please do) \
+You can do that by using my social networks. Never in person or I will have a panic attack, thank you.');
